b05c3ad11d66e6d46d7153e5b972583a02d4dc14
Supriya Maddu <117802859+SupriyaMaddu@users.noreply.github.com>
2023-08-07 20:37:28 +0530

    Add files via upload

---
+++ stringConcatenation.py
@@ -0,0 +1,11 @@
+word1 = input()
+word2 = input()
+print(word1 + word2)
+
+word1 = input()
+word2 = input()
+print(word1 + " " + word2)
+
+name = input()
+age = input()
+print(name + " is " + age + " years old")
